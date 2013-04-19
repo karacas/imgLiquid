186869a579b4dcb1fb7065b15e15e53d790f70b6
unknown <alejandroe@gmail.com>
2013-04-19 14:30:14 -0300

    0.9.7

--- src/js/imgLiquid-min.js
+++ src/js/imgLiquid-min.js
@@ -1,5 +1,5 @@
 /*
-jQuery Plugin: imgLiquid v0.9.65 / 19-04-13
+jQuery Plugin: imgLiquid v0.9.7 / 19-04-13
 jQuery plugin to resize images to fit in a container.
 Copyright (c) 2012 Alejandro Emparan (karacas), twitter: @krc_ale
 Dual licensed under the MIT and GPL licenses
