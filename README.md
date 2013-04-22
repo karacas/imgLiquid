9c4b9696f0e1b61777a1dd7a5035b9816497541a
unknown <alejandroe@gmail.com>
2013-04-22 17:25:49 -0300

    0.9.84 / Lots of changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![ScreenShot](https://raw.github.com/karacas/imgLiquid/master/examples/logoimgliquid.png)
 
-imgLiquid.js v0.9.75 19-04-13
+imgLiquid.js v0.9.84 22-04-13
 #####A jQuery Plugin to resize images to fit in a container.
 Alejandro Emparan (karacas) @krc_ale
 Dual licensed under the MIT and GPL licenses.
@@ -14,7 +14,7 @@ Dual licensed under the MIT and GPL licenses.
 
 ####js:
 ```js
-$(document).ready(function() {
+$(function() {
 	$(".imgLiquid").imgLiquid();
 });
 ```
@@ -46,41 +46,36 @@ http://codepen.io/karacas/pen/nlugd
 ## 
 ###Features:
 ```
-	- Lightweight: less than 2KBs gzipped.
-	- Align.
+	- Uses CSS Background-size when is available. (new)
+    - Bootstrap compatible
+    - Lightweight: 2.0KBs gzipped.
 	- Fill/Crop.
-	- Responsive (Optional, default is off).
+    - Align.
+	- Responsive.
 	- Svg support.
 	- CallBacks.
-	- FadeIn Anim (Optional, default is off).
+    - HTML5 data-* attributes
 	- All browsers (Incl. ie6).
 ```
  
 ###Options:
 ```
+
     >js
         fill: true,
-        verticalAlign: 		//'center' //'top' 	//'bottom'
-        horizontalAlign: 	//'center' //'left'	//'right'
-        fadeInTime: 0,
-        delay: 0,			//time to process next image in milliseconds
-        responsive: false,
-        responsiveCheckTime: 500, 	//time to check resize in milliseconds
+        verticalAlign:      //'center' //'top' //'bottom'
+        horizontalAlign:    //'center' //'left' //'right'
 
-	>js callBakcs
-		onStart:		function(){},
-		onFinish:		function(){},
-		onItemResize:	function(index, container, img){},
-		onItemStart:	function(index, container, img){},
-		onItemFinish:	function(index, container, img){}
+    >js callBakcs
+        onStart:        function(){},
+        onFinish:       function(){},
+        onItemResize:   function(index, container, img){},
+        onItemStart:    function(index, container, img){},
+        onItemFinish:   function(index, container, img){}
 
-	>css (set useCssAligns: true) (overwrite js)
-		text-align: center;
-		vertical-align : middle;
+    >hml5 data attr (overwrite js options)
+        data-imgLiquid-fill="true"
+        data-imgLiquid-horizontalAlign="center"
+        data-imgLiquid-verticalAlign="center"
 
-	>hml5 data attr (overwrite all)
-		data-imgLiquid-fill="true"
-		data-imgLiquid-horizontalAlign="center"
-		data-imgLiquid-verticalAlign="center"
-		data-imgLiquid-fadeInTime="500"
 ```
--- src/js/imgLiquid.js
+++ src/js/imgLiquid.js
@@ -1,5 +1,5 @@
 /*
-jQuery Plugin: imgLiquid v0.9.75 / 19-04-13
+jQuery Plugin: imgLiquid v0.9.84 / 22-04-13
 jQuery plugin to resize images to fit in a container.
 Copyright (c) 2012 Alejandro Emparan (karacas), twitter: @krc_ale
 Dual licensed under the MIT and GPL licenses
@@ -14,263 +14,305 @@ ex:
 		fill: true,
 		verticalAlign:		//'center' //'top' //'bottom'
 		horizontalAlign:	//'center' //'left' //'right'
-		fadeInTime: 0,
-		delay: 0,			//time to process next image in milliseconds
-		responsive: false,
-		responsiveCheckTime: 500,	//time to check resize in milliseconds
-
 
 	>js callBakcs
 		onStart:		function(){},
 		onFinish:		function(){},
-		onItemResize:   function(index, container, img){},
+		onItemResize: function(index, container, img){},
 		onItemStart:	function(index, container, img){},
-		onItemFinish:   function(index, container, img){}
-
-
-	>css (set useCssAligns: true) (overwrite js)
-		text-align: center;
-		vertical-align : middle;
-
+		onItemFinish: function(index, container, img){}
 
 	>hml5 data attr (overwrite all)
 		data-imgLiquid-fill="true"
 		data-imgLiquid-horizontalAlign="center"
 		data-imgLiquid-verticalAlign="center"
-		data-imgLiquid-fadeInTime="500"
+
 */
 //
-;(function($){
+
+var imgLiquid = imgLiquid || {VER: '0.9.84'};
+
+(function ($) {
+
+
+	imgLiquid.isIE = /*@cc_on!@*/ false;
+	imgLiquid.backgroundSizeAvaiable = false;
+
+	//___________________________________________________________________
+
+	function checkbackgroundSize() {
+		$('body').append('<span id="imgLiquidTest" style="background-size:cover"></span>');
+		if ($('#imgLiquidTest').css('background-size') === 'cover') imgLiquid.backgroundSizeAvaiable = true;
+		$('#imgLiquidTest').remove();
+	}
+	$(checkbackgroundSize);
+
+
+	//___________________________________________________________________
 	$.fn.extend({
-		imgLiquid: function(options) {
+		imgLiquid: function (options) {
 
-			//is ie?
-			var isIE = /*@cc_on!@*/false;
+			var self = this;
+			this.defaultOptions = {};
 
-
-			//Sizes
 			var totalItems = this.length;
 			var processedItems = 0;
 
 
-			//Settings - Options
-			this.defaultOptions = {};
 
-
-			var self = this;
-
-
-			//___________________________________________________________________
-			var settings = $.extend({
-
+			this.defaults = {
 				fill: true,
-				verticalAlign: 'center',	// 'top'	// 'bottom'
-				horizontalAlign: 'center',	// 'left'	// 'right'
-				fadeInTime: 0,
-				responsive: false,
-				responsiveCheckTime: 100,  /*time to check div resize, default 10fps > 1000/100*/
-				delay: 0,
+				verticalAlign: 'center',		// 'top'	// 'bottom'
+				horizontalAlign: 'center',		// 'left'	// 'right'
 
-				removeBoxBackground: true,
-				ieFadeInDisabled: true,
+				useBackgroundSize: true,
 				useDataHtmlAttr: true,
-				useCssAligns: false,
-				imageRendering: 'auto',
-				hardPixels: false,
-				checkvisibility: true,
-				timecheckvisibility : 250,
+
+				responsive: true,				/*Only for use with BackgroundSize false (or old browsers)*/
+				delay: 0,						/*Only for use with BackgroundSize false (or old browsers)*/
+				fadeInTime: 0,					/*Only for use with BackgroundSize false (or old browsers)*/
+				removeBoxBackground: true,		/*Only for use with BackgroundSize false (or old browsers)*/
+				ieFadeInDisabled: true,			/*Only for use with BackgroundSize false (or old browsers)*/
+				hardPixels: true,				/*Only for use with BackgroundSize false (or old browsers)*/
+				responsiveCheckTime: 500,		/*Only for use with BackgroundSize false (or old browsers)*/ /*time to check div resize*/
+				timecheckvisibility: 500,		/*Only for use with BackgroundSize false (or old browsers)*/ /*time to recheck if visible/loaded*/
 
 				//CALLBACKS
-				onStart: null,		//no-params
-				onFinish: null,		//no-params
+				onStart: null, //no-params
+				onFinish: null, //no-params
 				onItemResize: null, //params: (index, container, img )
-				onItemStart: null,	//params: (index, container, img )
-				onItemFinish: null	//params: (index, container, img )
+				onItemStart: null, //params: (index, container, img )
+				onItemFinish: null //params: (index, container, img )
+			};
 
-			}, this.defaultOptions, options);
+
+
+			this.settings = {};
+			this.options = {};
+			$.extend(this.options, options);
+			$.extend(this.settings, this.defaults, options);
 
 
 
 			//CALLBACK > Start
-			if (settings.onStart) settings.onStart();
+			if (this.settings.onStart) this.settings.onStart();
 
-
-
-			//SAVES NEW SETTINGS
-			if (self.data('settings') !== undefined)  {
-				var settTmp	= $.extend(self.data('settings'));
-				settTmp		= $.extend(options);
-				self.data('settings',  settTmp);
-			}
 
 
 			//___________________________________________________________________
-			return this.each(function($i) {
 
-				//Obj
+			return this.each(function ($i) {
+
+				//OBJ
 				var $imgBoxCont = $(this);
 				var $img = $('img:first', $imgBoxCont);
-
-				if (!$img || $img === null || $img.size() ===0){
+				if (!$img || $img === null || $img.length === 0) {
 					onError();
-					return null;
-				}
-
-				if ($img.data('ILprocessed')){
-					settings = $.extend(self.data('settings'));
-					process($imgBoxCont, $img, $i);
 					return;
 				}
 
 
-				//STATUS
-				$img.data('ILprocessed', false);
-				$img.ILerror = false;
-
-
-				//CALLBACK > ItemStart (index, container, img )
-				if (settings.onItemStart) settings.onItemStart($i , $imgBoxCont , $img);
-
-
-				//Alpha to 0 & removes
-				$img.fadeTo(0, 0);
-				$('img:not(:first)', $imgBoxCont).css('display','none');
-				$img.css({'visibility':'visible', 'max-width':'none', 'max-height':'none', 'width':'auto', 'height':'auto', 'display':'block', 'image-rendering':settings.imageRendering });
-				$img.removeAttr("width");
-				$img.removeAttr("height");
-
-
-				//Delay > 1
-				if (settings.delay <1) settings.delay = 1;
-
-
-				//set OverFlow
-				$imgBoxCont.css({'overflow':'hidden'});
-
-
-				//SETTINGS OVERWRITE
-				if (isIE && settings.imageRendering === 'optimizeQuality') $img.css('-ms-interpolation-mode', 'bicubic');
-				if (settings.useCssAligns) {
-					var cha = $imgBoxCont.css('text-align');
-					var cva = $imgBoxCont.css('vertical-align');
-					if(cha === 'left' || cha === 'center' || cha === 'right') settings.horizontalAlign = cha;
-					if (cva === 'top' || cva === 'middle' || cva === 'bottom' || cva === 'center') settings.verticalAlign = cva;
+				//EXTEND OPTIONS
+				var settings = {};
+				if ($img.data('imgLiquid_settings')) {
+					//Recall
+					$.extend(settings, $img.data('imgLiquid_settings'), self.options);
+				} else {
+					//1st time
+					$.extend(settings, self.settings, self.options);
 				}
-				if (settings.useDataHtmlAttr) {
-					if ($imgBoxCont.attr('data-imgLiquid-fill') === 'true') settings.fill = true;
-					if ($imgBoxCont.attr('data-imgLiquid-fill') === 'false' ) settings.fill = false;
-					if ($imgBoxCont.attr('data-imgLiquid-responsive') === 'true') settings.responsive = true;
-					if ($imgBoxCont.attr('data-imgLiquid-responsive') === 'false' ) settings.responsive = false;
-					if ( Number ($imgBoxCont.attr('data-imgLiquid-fadeInTime')) > 0) settings.fadeInTime = Number ($imgBoxCont.attr('data-imgLiquid-fadeInTime'));
-					var ha = $imgBoxCont.attr('data-imgLiquid-horizontalAlign');
-					var va = $imgBoxCont.attr('data-imgLiquid-verticalAlign');
-					if (ha === 'left' || ha === 'center' || ha === 'right') settings.horizontalAlign = ha;
-					if (va === 'top' || va === 'middle' || va === 'bottom' || va === 'center') settings.verticalAlign = va;
+				$img.data('imgLiquid_settings', settings);
+
+
+
+				//CHECK REPROCESS
+				if ($img.data('imgLiquid_Processed')) {
+					//NEW
+					setWithbackgroundSize();
+					return;
+				}
+				if ($img.data('imgLiquid_oldProcessed')) {
+					//OLD
+					if (!(imgLiquid.backgroundSizeAvaiable && settings.useBackgroundSize)) oldProcess();
+					return;
 				}
 
 
-				//ie no anims > (muere ie, muere!)
-				if (isIE && settings.ieFadeInDisabled) settings.fadeInTime = 0;
+
+
+				$img.data('imgLiquid_oldProcessed', false);
+				$img.data('imgLiquid_error', false);
+				setSettingsOverwrite();
+				if (settings.onItemStart) settings.onItemStart($i, $imgBoxCont, $img);
 
 
 
-				//RESPONSIVE
-				function checkElementSize(){
-					setTimeout(checkElementSizeDelay, settings.responsiveCheckTime);
-				}
-				function checkElementSizeDelay(){
-
-					//UPDATE SETTINGS
-					settings = $.extend(self.data('settings'));
-
-					$imgBoxCont.actualSize = $imgBoxCont.get(0).offsetWidth + ($imgBoxCont.get(0).offsetHeight/100000);
-					if ($imgBoxCont.actualSize !== $imgBoxCont.sizeOld){
-						if ($img.data('ILprocessed') && $imgBoxCont.sizeOld !== undefined){
-
-							//CALLBACK > onItemResize (index, container, img )
-							if (settings.onItemResize) settings.onItemResize($i , $imgBoxCont , $img);
-
-							//Process again
-							if (settings.responsive) process($imgBoxCont, $img, $i);
-						}
-					}
-					$imgBoxCont.sizeOld = $imgBoxCont.actualSize;
-					checkElementSize();
-
-				}
-				if (settings.responsive) settings.hardPixels = true;
-				if (settings.responsive || settings.onItemResize !== null) checkElementSize();
-
-
-				//SAVE FIRST TIME SETTINGS
-				self.data('settings', settings);
-
-				//LOAD
-				$img.on('load', onLoad).on('error', onError);
-				if($img[0].complete)$img.load();
-
-
-				function onLoad(e){
-					if (!Boolean($img[0].width === 0 && $img[0].height === 0)) {
-						if (settings.checkvisibility){
-							checkProcess();
-						}else{
-							//DIRECT > OLD VERSION TEST AND DELETE
-							setTimeout(function() {
-								process($imgBoxCont, $img, $i);
-							}, $i * settings.delay);
-						}
-					}
-					if (e) e.preventDefault();
-				}
-				function onError(){
-					$img.ILerror = true;
-					checkFinish($imgBoxCont, $img, $i);
-					$imgBoxCont.css('visibility', 'hidden');
-				}
-				function checkProcess(){
-					if ($img.data('ILprocessed')) return;
-					setTimeout(function() {
-						if ($imgBoxCont.is(':visible')){
-							setTimeout(function() {
-								process($imgBoxCont, $img, $i);
-							}, $i * settings.delay);
-						}else{
-							checkProcess();
-						}
-					}, settings.timecheckvisibility);
+				if (imgLiquid.backgroundSizeAvaiable && settings.useBackgroundSize) {
+					//New browsers with backgroundSize
+					setWithbackgroundSize();
+				} else {
+					//Old browsers whitout backgroundSize
+					setCss();
+					onLoad();
+					checkResponsive();
 				}
 
 
 
 				//___________________________________________________________________
-				function process($imgBoxCont, $img, $i){
 
-					//RESIZE
-					var w,h;
-					if ($img.data('owidth') === undefined)  $img.data('owidth', $img[0].width);
-					if ($img.data('oheight') === undefined) $img.data('oheight', $img[0].height);
-					if (settings.fill === ($imgBoxCont.width() / $imgBoxCont.height()) >= ($img.data('owidth') / $img.data('oheight'))){
-						w = '100%'; h = 'auto';
-						if (settings.hardPixels){
-							w = Math.floor ($imgBoxCont.width());
-							h = Math.floor ($imgBoxCont.width() * ($img.data('oheight') / $img.data('owidth')));
-						}
-					}else{
-						h = '100%'; w = 'auto';
-						if (settings.hardPixels){
-							h = Math.floor ($imgBoxCont.height());
-							w = Math.floor ($imgBoxCont.height() * ($img.data('owidth') / $img.data('oheight')));
+				function setWithbackgroundSize() {
+					var bsVale = (settings.fill) ? 'cover' : 'contain';
+					var bpos = settings.horizontalAlign.toLowerCase() + " " + settings.verticalAlign.toLowerCase();
+					$imgBoxCont.css({
+						'background-size': bsVale,
+						'background-repeat': 'no-repeat',
+						'background-image': 'url(' + $img.attr('src') + ')',
+						'background-position': bpos
+					});
+					$('a:first', $imgBoxCont).css({
+						'display': 'block',
+						'width': '100%',
+						'height': '100%'
+					});
+					$('img', $imgBoxCont).css({
+						'display': 'none'
+					});
+					if (settings.onItemFinish) settings.onItemFinish($i, $imgBoxCont, $img);
+					checkFinish();
+					$img.data('imgLiquid_Processed', true);
+				}
+
+
+
+
+
+				//___________________________________________________________________
+
+				function setCss() {
+					$img.fadeTo(0, 0);
+					$('img:not(:first)', $imgBoxCont).css('display', 'none');
+					$img.removeAttr("width").removeAttr("height").css({
+						'visibility': 'visible',
+						'max-width': 'none',
+						'max-height': 'none',
+						'width': 'auto',
+						'height': 'auto',
+						'display': 'block'
+					});
+					$imgBoxCont.css({
+						'overflow': 'hidden'
+					});
+				}
+
+
+
+
+				//___________________________________________________________________
+
+				function setSettingsOverwrite() {
+					if (settings.useDataHtmlAttr) {
+						if ($imgBoxCont.attr('data-imgLiquid-fill') === 'true') settings.fill = true;
+						if ($imgBoxCont.attr('data-imgLiquid-fill') === 'false') settings.fill = false;
+						if ($imgBoxCont.attr('data-imgLiquid-responsive') === 'true') settings.responsive = true;
+						if ($imgBoxCont.attr('data-imgLiquid-responsive') === 'false') settings.responsive = false;
+						if (Number($imgBoxCont.attr('data-imgLiquid-fadeInTime')) > 0) settings.fadeInTime = Number($imgBoxCont.attr('data-imgLiquid-fadeInTime'));
+
+						var ha = $imgBoxCont.attr('data-imgLiquid-horizontalAlign');
+						var va = $imgBoxCont.attr('data-imgLiquid-verticalAlign');
+						if (ha === 'left' || ha === 'center' || ha === 'right') settings.horizontalAlign = ha;
+						if (va === 'top' || va === 'middle' || va === 'bottom' || va === 'center') settings.verticalAlign = va;
+					}
+
+					//ie no anims
+					if (imgLiquid.isIE && settings.ieFadeInDisabled) settings.fadeInTime = 0;
+				}
+
+
+
+
+				//___________________________________________________________________
+
+				function checkResponsive() {
+					if (!settings.responsive && !$img.data('imgLiquid_oldProcessed')) return;
+					$.extend(settings, $img.data('imgLiquid_settings'));
+
+					$imgBoxCont.actualSize = $imgBoxCont.get(0).offsetWidth + ($imgBoxCont.get(0).offsetHeight / 100000);
+					if ($imgBoxCont.sizeOld) {
+						if ($imgBoxCont.actualSize !== $imgBoxCont.sizeOld) {
+							oldProcess();
+							//CALLBACK
+							if (settings.onItemResize) settings.onItemResize($i, $imgBoxCont, $img);
 						}
 					}
-					$img.css({'width':w, 'height':h});
+					$imgBoxCont.sizeOld = $imgBoxCont.actualSize;
+					setTimeout(checkResponsive, settings.responsiveCheckTime);
+				}
+
+
+
+
+
+				//___________________________________________________________________
+
+				function onLoad() {
+					if ($img.data('imgLiquid_loaded') || $img.data('imgLiquid_oldProcessed')) return;
+					if ($imgBoxCont.is(':visible') && $img[0].complete && $img[0].complete && $img[0].width > 0 && $img[0].height > 0) {
+						$img.data('imgLiquid_loaded', true);
+						setTimeout(oldProcess, $i * settings.delay);
+					} else {
+						setTimeout(onLoad, settings.timecheckvisibility);
+					}
+				}
+
+
+
+
+				//___________________________________________________________________
+
+				function onError() {
+					$img.data('imgLiquid_error', true);
+					$imgBoxCont.css('visibility', 'hidden');
+					checkFinish();
+				}
+
+
+
+
+				//___________________________________________________________________
+
+				function oldProcess() {
+
+					//RESIZE
+					var w, h;
+					if ($img.data('owidth') === undefined) $img.data('owidth', $img[0].width);
+					if ($img.data('oheight') === undefined) $img.data('oheight', $img[0].height);
+					if (settings.fill === ($imgBoxCont.width() / $imgBoxCont.height()) >= ($img.data('owidth') / $img.data('oheight'))) {
+						w = '100%';
+						h = 'auto';
+						if (settings.hardPixels) {
+							w = Math.floor($imgBoxCont.width());
+							h = Math.floor($imgBoxCont.width() * ($img.data('oheight') / $img.data('owidth')));
+						}
+					} else {
+						h = '100%';
+						w = 'auto';
+						if (settings.hardPixels) {
+							w = Math.floor($imgBoxCont.height() * ($img.data('owidth') / $img.data('oheight')));
+							h = Math.floor($imgBoxCont.height());
+						}
+					}
+					$img.css({
+						'width': w,
+						'height': h
+					});
 
 
 					//align X
 					var ha = settings.horizontalAlign.toLowerCase();
 					var hdif = $imgBoxCont.width() - $img[0].width;
 					var margL = 0;
-					if (ha === 'center' || ha === 'middle')margL = hdif/2;
+					if (ha === 'center' || ha === 'middle') margL = hdif / 2;
 					if (ha === 'right') margL = hdif;
 					$img.css('margin-left', Math.floor(margL));
 
@@ -279,35 +321,29 @@ ex:
 					var va = settings.verticalAlign.toLowerCase();
 					var vdif = $imgBoxCont.height() - $img[0].height;
 					var margT = 0;
-					if (va === 'center' || va === 'middle') margT = vdif/2;
+					if (va === 'center' || va === 'middle') margT = vdif / 2;
 					if (va === 'bottom') margT = vdif;
 					$img.css('margin-top', Math.floor(margT));
 
 
 					//FadeIn
-					if (!$img.data('ILprocessed')){
+					if (!$img.data('imgLiquid_oldProcessed')) {
 						if (settings.removeBoxBackground) $imgBoxCont.css('background-image', 'none');
 						$img.fadeTo(settings.fadeInTime, 1);
-						$img.data('ILprocessed', true) ;
-
-						//CALLBACK > onItemFinish (index, container, img )
-						if (settings.onItemFinish) settings.onItemFinish($i , $imgBoxCont , $img);
-
-						checkFinish($imgBoxCont, $img, $i);
+						$img.data('imgLiquid_oldProcessed', true);
+						if (settings.onItemFinish) settings.onItemFinish($i, $imgBoxCont, $img);
+						checkFinish();
 					}
 				}
 
 
 
 
-
 				//___________________________________________________________________
-				function checkFinish($imgBoxCont, $img, $i){
-					processedItems ++;
-					//CALLBACK > onFinish
-					if (processedItems === totalItems){
-						if (settings.onFinish) settings.onFinish();
-					}
+
+				function checkFinish() {
+					processedItems++;
+					if (processedItems === totalItems) if (settings.onFinish) settings.onFinish();
 				}
 
 
